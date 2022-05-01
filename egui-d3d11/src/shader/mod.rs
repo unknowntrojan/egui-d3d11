b86125da883b2db5777ec9fc8a3b7bc572f8b3bc
sy1ntexx <sy1ntexx@gmail.com>
2022-05-01 18:59:33 +0300

    Added static complied shaders

--- egui-d3d11/src/shader/mod.rs
+++ egui-d3d11/src/shader/mod.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use windows::Win32::{
     Foundation::PSTR,
     Graphics::{
@@ -13,18 +15,28 @@ trait Shader {
     const ENTRY: PSTR;
     const TARGET: PSTR;
 
-    unsafe fn create_shader(device: &ID3D11Device, blob: &ID3DBlob) -> Self;
+    unsafe fn create_shader(device: &ID3D11Device, blob: &ShaderData) -> Self;
 }
 
 impl Shader for ID3D11VertexShader {
     const ENTRY: PSTR = p_str!("vs_main");
     const TARGET: PSTR = p_str!("vs_5_0");
 
-    unsafe fn create_shader(device: &ID3D11Device, blob: &ID3DBlob) -> Self {
-        expect!(
-            device.CreateVertexShader(blob.GetBufferPointer(), blob.GetBufferSize(), None),
-            "Failed to create vertex shader"
-        )
+    unsafe fn create_shader(device: &ID3D11Device, blob: &ShaderData) -> Self {
+        match blob {
+            ShaderData::EmbeddedData(arr) => {
+                expect!(
+                    device.CreateVertexShader(arr.as_ptr() as _, arr.len() as _, None),
+                    "Failed to create vertex shader"
+                )
+            }
+            ShaderData::CompiledBlob(blob) => {
+                expect!(
+                    device.CreateVertexShader(blob.GetBufferPointer(), blob.GetBufferSize(), None),
+                    "Failed to create vertex shader"
+                )
+            }
+        }
     }
 }
 
@@ -32,38 +44,117 @@ impl Shader for ID3D11PixelShader {
     const ENTRY: PSTR = p_str!("ps_main");
     const TARGET: PSTR = p_str!("ps_5_0");
 
-    unsafe fn create_shader(device: &ID3D11Device, blob: &ID3DBlob) -> Self {
-        expect!(
-            device.CreatePixelShader(blob.GetBufferPointer(), blob.GetBufferSize(), None),
-            "Failed to create vertex shader"
-        )
+    unsafe fn create_shader(device: &ID3D11Device, blob: &ShaderData) -> Self {
+        match blob {
+            ShaderData::EmbeddedData(arr) => {
+                expect!(
+                    device.CreatePixelShader(arr.as_ptr() as _, arr.len() as _, None),
+                    "Failed to create vertex shader"
+                )
+            }
+            ShaderData::CompiledBlob(blob) => {
+                expect!(
+                    device.CreatePixelShader(blob.GetBufferPointer(), blob.GetBufferSize(), None),
+                    "Failed to create vertex shader"
+                )
+            }
+        }
+    }
+}
+
+pub enum ShaderData {
+    EmbeddedData(&'static [u8]),
+    CompiledBlob(ID3DBlob),
+}
+
+impl ShaderData {
+    #[inline]
+    pub fn as_ptr(&self) -> *mut () {
+        match self {
+            ShaderData::EmbeddedData(arr) => (*arr).as_ptr() as _,
+            ShaderData::CompiledBlob(b) => unsafe {
+                b.GetBufferPointer() as _
+            },
+        }
+    }
+    
+    #[inline]
+    pub fn len(&self) -> usize {
+        match self {
+            ShaderData::EmbeddedData(arr) => (*arr).len(),
+            ShaderData::CompiledBlob(b) => unsafe {
+                b.GetBufferSize()
+            },
+        }
     }
 }
 
 pub struct CompiledShaders {
     pub vertex: ID3D11VertexShader,
     pub pixel: ID3D11PixelShader,
-    cache: ID3DBlob,
+    cache: ShaderData,
 }
 
 impl CompiledShaders {
     pub fn new(device: &ID3D11Device) -> Self {
-        let (cache, vertex) = Self::compile_shader::<ID3D11VertexShader>(device);
-        let (_, pixel) = Self::compile_shader::<ID3D11PixelShader>(device);
+        if cfg!(feature = "force-compile") {
+            let (vcache, vertex) = Self::compile_shader::<ID3D11VertexShader>(device);
+            let (_pcache, pixel) = Self::compile_shader::<ID3D11PixelShader>(device);
 
-        Self {
-            vertex,
-            pixel,
-            cache,
+            if cfg!(feature = "save-blob") {
+                unsafe {
+                    std::fs::OpenOptions::new()
+                        .write(true)
+                        .read(true)
+                        .create(true)
+                        .open("vertex.bin")
+                        .unwrap()
+                        .write_all(std::slice::from_raw_parts(
+                            vcache.GetBufferPointer() as *mut u8,
+                            vcache.GetBufferSize(),
+                        ))
+                        .unwrap();
+                        
+                    std::fs::OpenOptions::new()
+                        .write(true)
+                        .read(true)
+                        .create(true)
+                        .open("pixel.bin")
+                        .unwrap()
+                        .write_all(std::slice::from_raw_parts(
+                            _pcache.GetBufferPointer() as *mut u8,
+                            _pcache.GetBufferSize(),
+                        ))
+                        .unwrap();
+                }
+            }
+
+            Self {
+                vertex,
+                pixel,
+                cache: ShaderData::CompiledBlob(vcache),
+            }
+        } else {
+            unsafe {
+                let cache = ShaderData::EmbeddedData(include_bytes!("vertex.bin"));
+                let vertex = ID3D11VertexShader::create_shader(device, &cache);
+                let pixel = ID3D11PixelShader::create_shader(device, &ShaderData::EmbeddedData(include_bytes!("pixel.bin")));
+
+                Self {
+                    cache,
+                    vertex,
+                    pixel
+                }
+            }
         }
     }
 
     pub fn bytecode_ptr(&self) -> *mut () {
-        unsafe { self.cache.GetBufferPointer() as _ }
+        self.cache.as_ptr()
     }
 
     pub fn bytecode_len(&self) -> usize {
-        unsafe { self.cache.GetBufferSize() }
+        self.cache.len()
     }
 
     fn compile_shader<S: Shader>(device: &ID3D11Device) -> (ID3DBlob, S) {
@@ -107,7 +198,7 @@ impl CompiledShaders {
             } else {
                 (
                     code.clone().unwrap(),
-                    S::create_shader(device, code.as_ref().unwrap()),
+                    S::create_shader(device, &ShaderData::CompiledBlob(code.unwrap())),
                 )
             }
         }
